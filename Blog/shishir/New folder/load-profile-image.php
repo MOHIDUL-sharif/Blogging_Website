49ed9dab85a6a9976220b8730e7776cc0f6ba8a0
Mohidul Alam <mohidul17@cse.pstu.ac.bd>
2023-05-18 00:00:34 +0600

    1st phase done

--- Blog/shishir/New folder/load-profile-image.php
+++
@@ -1,7 +0,0 @@
-<?php   
-  $conn = mysqli_connect("localhost", "root", "", "searchbuy");
-  $query = "SELECT * FROM users INNER JOIN userdetails ON users.email = userdetails.email AND users.uid='" . $_SESSION['adminSession'] . "'";
-  $row = mysqli_query($conn, $query);
-  $cust = mysqli_fetch_array($row);
-  $sidebarImage = '../Asset/image/admin/'.$cust["photo"];
-?>
--- Blog/shishir/New folder/sessionCheck.php
+++
@@ -1,7 +0,0 @@
-
-<?php
-session_start();
-if (!isset($_SESSION['adminSession'])) {
-  header("location: ./login.php");
-}
-?>
